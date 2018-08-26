0dcce697c801598892efe115063b4e61be426c52
op-x <441294402@qq.com>
2018-08-26 10:30:59 +0800

    shell

--- shell/memlog.sh
+++ shell/memlog.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 time=`date +%Y-%m-%d__%H:%M:%S` # 当前时间
-total=(`free -m | awk '{printf $2" "}'`) # 总量
-free=(`free -m | awk '{print $4" "}'`) # 剩余量
-current_occupancy=$[$[${total[1]}-${free[1]}]*100/${total[1]}] # 当前占用
+total=(`free -m | head -n 2 | tail -n 1 | awk '{printf $2" "$4}'`) # 总量 剩余量
+current_occupancy=$[$[${total[0]}-${total[1]}]*100/${total[0]}] # 当前占用
 dynamic=$(echo "scale=1;0.3*$1+0.7*$current_occupancy" | bc) # 占用百分比动态平均值
 
-echo $time ${total[1]}M ${free[1]}M $current_occupancy% $dynamic%
+echo $time ${total[0]}M ${total[1]}M $current_occupancy% $dynamic%
