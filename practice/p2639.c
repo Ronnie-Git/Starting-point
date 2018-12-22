96ec66921a2da440e372ac62b0797a357d00fcc5
op-x <441294402@qq.com>
2018-12-22 18:51:36 +0800

    add p2639.c

--- practice/p2639.c
+++ practice/p2639.c
@@ -2,29 +2,26 @@
 	> File Name: p2639.c
 	> Author: x
 	> Mail: x.com
-	> Created Time: 2018年12月22日 星期六 12时54分53秒
+	> Created Time: 2018年12月22日 星期六 18时46分46秒
  ************************************************************************/
 
 #include <stdio.h>
 
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 int main() {
-    int n, m, num[505] = {0}, ans[50000] = {0};
-    ans[0] = 1;
+    int m, n, num[505] = {0}, dp[45005] = {0};
     scanf("%d %d", &m, &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &num[i]);
     }
-    
+
     for (int i = 0; i < n; i++) {
         for (int j = m; j >= num[i]; j--) {
-            if (ans[j - num[i]] == 1) ans[j] = 1;
+            dp[j] = max(dp[j], dp[j - num[i]] + num[i]);
         }
     }
-    for (int i = m; i >= 0; i--) {
-        if (ans[i]) {
-            printf("%d\n", i);
-            break;
-        }
-    }
+
+    printf("%d\n", dp[m]);
     return 0;
 }
