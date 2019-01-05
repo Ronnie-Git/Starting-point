da868cb39060bb9c1edd6cead4f0454357169d11
op-x <441294402@qq.com>
2019-01-05 15:10:23 +0800

    add 28.c

---
+++ LeetCode/28.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: 28.c
+	> Author: x
+	> Mail: x.com
+	> Created Time: 2019年01月05日 星期六 15时10分00秒
+ ************************************************************************/
+
+int strStr(char* haystack, char* needle) {
+    if (strlen(needle) == 0) return 0;
+    int *next = (int *)malloc(sizeof(int) * strlen(needle));
+    next[0] = -1;
+    for (int i = 1; needle[i]; i++) {
+        int j = next[i - 1];
+        while (j != -1 && needle[j + 1] != needle[i]) j = next[j];
+        if (needle[j + 1] == needle[i]) next[i] = j + 1;
+        else next[i] = -1;
+    }
+    int i = 0, j = -1;
+    while (haystack[i]) {
+        while (j != -1 && haystack[i] != needle[j + 1]) j = next[j];
+        if (haystack[i] == needle[j + 1]) {
+            j += 1;
+        }
+        i += 1;
+        if (needle[j + 1] == 0) return i - strlen(needle);
+    }
+    return -1;
+}
