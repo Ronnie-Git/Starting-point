8b8bf582b297ed6ed3ba38df0c22b5b30125a796
op-x <441294402@qq.com>
2019-01-13 22:38:37 +0800

    add 165.c

--- LeetCode/165.c
+++ LeetCode/165.c
@@ -5,29 +5,23 @@
 	> Created Time: 2019年01月13日 星期日 19时50分06秒
  ************************************************************************/
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-
-
 int compareVersion(char* version1, char* version2) {
-    while (str1 && str2) {
-        int temp1 = atoi(str1), temp2 = atoi(str2);
-        printf("%d %d\n", temp1, temp2);
+    int i = -1, j = -1;
+    int len1 = strlen(version1), len2 = strlen(version2);
+    do {
+        if (i < len1) ++i;
+        if (j < len2) ++j;
+        int temp1 = 0, temp2 = 0;
+        while (version1[i] && version1[i] != '.') {
+            temp1 = temp1 * 10 + (version1[i] - '0');
+            i++;
+        }
+        while (version2[j] && version2[j] != '.') {
+            temp2 = temp2 * 10 + (version2[j] - '0');
+            j++;
+        }
         if (temp1 > temp2) return 1;
         if (temp1 < temp2) return -1;
-        str1 = strtok(str1, ".");
-        str2 = strtok(str2, ".");
-    }
-    if (str1 == str2) return 0;
-    return str1 ? 1 : -1;
-}
-
-int main() {
-    char str1[100] = {0}, str2[100] = {0};
-    scanf("%s", str1);
-    scanf("%s", str2);
-    printf("%d\n", compareVersion(str1, str2));
+    } while (version1[i] || version2[j]);
     return 0;
 }
