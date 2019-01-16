aa37e13c24225fb264e3e1bc301aa0b62710d2ae
op-x <441294402@qq.com>
2019-01-16 11:43:25 +0800

    add 5.c_inherit/

---
+++ practice/4.c++/5.c_inherit/head.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author: x
+	> Mail: x.com
+	> Created Time: 2019年01月16日 星期三 11时33分04秒
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+typedef void demo;
+typedef void deverid;
+
+demo* demo_init(int i, int j);
+int getA(demo *pthis);
+int getB(demo *pthis);
+int add(demo *pthis, int value);
+void free_demo(demo *pthis);
+
+deverid* deverid_init(int i, int j, int k);
+int getC(deverid *pthis);
+int add1(deverid *pthi, int value);
+void free_deverid(deverid *pthis);
+#endif
