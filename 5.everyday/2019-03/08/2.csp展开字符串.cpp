7d408573fc5fb0e1802de80b1a1eba87b1b54fbb
op-x <441294402@qq.com>
2019-03-08 22:22:33 +0800

    add 2.csp展开字符串.cpp

---
+++ 5.everyday/2019-03/08/2.csp展开字符串.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+	> File Name: 1.字符串.cpp
+	> Author: x
+	> Mail: x.com
+	> Created Time: 2019年03月08日 星期五 20时05分24秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int output(char *str) {
+    int cnt = 0, i = 0; // cnt记录字符串循环次数
+    while (str[i]) {
+        if (str[i] == ')') return i;
+        
+        // 遇到 ( 进入递归
+        if (str[i] == '(') {
+            if (cnt == 0) cnt = 1; // 有可能()前没数字（猜测）
+            int ind = 0;
+            for (int j = 0; j < cnt; j++) {
+                ind = output(str + i + 1);
+            }
+            cnt = 0;
+            i = i + ind + 2;
+            continue;
+        }
+        
+        if (str[i] >= '0' && str[i] <= '9') {
+            // 根据题意有可能循环次数不只是一位数，但int能存下
+            cnt = cnt * 10 + (str[i] - '0'); 
+        } else if (cnt != 0) {
+            // 没有() 只循环一个字母
+            for (int j = 0; j < cnt; j++) cout << str[i];
+            cnt = 0;
+        } else {
+            cout << str[i];
+        }
+
+        i++;
+    }
+    return i;
+}
+
+int main() {
+    char str[10000] = {0};
+    while (scanf("%s", str) != EOF) {
+        output(str);
+        cout << endl;
+        memset(str, 0, sizeof(str));
+    }
+    return 0;
+}
