27aa3fcfb84a802e967a4a0bbacfccb38a382357
opx <linux.com>
2019-03-17 11:01:29 +0800

    add h.cpp

---
+++ 4.practice/1.others/h.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: h.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun Mar 17 11:00:46 2019
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
